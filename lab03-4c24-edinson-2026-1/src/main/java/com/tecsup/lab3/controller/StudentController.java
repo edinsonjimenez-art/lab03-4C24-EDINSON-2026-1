a6391a14fbd6de9bf9e5be7e2714987badcdbff6
Edinson Jimenez <edinson.jimenez@tecsup.edu.pe>
2026-04-01 20:20:01 -0500

    StudentController sprint-2

---
+++ lab03-4c24-edinson-2026-1/src/main/java/com/tecsup/lab3/controller/StudentController.java
@@ -0,0 +1,9 @@
+package com.tecsup.lab3.controller;
+
+public class StudentController {
+
+    public void mostrar(){
+        System.out.println("Sprint 2 - Colaborador");
+    }
+
+}
---
+++ lab03-4c24-edinson-2026-1/src/main/java/com/tecsup/lab3/controller/StudentController2.java
@@ -0,0 +1,9 @@
+package com.tecsup.lab3.controller;
+
+public class StudentController2 {
+
+    public void mostrar(){
+        System.out.println("Sprint 2 - Colaborador");
+    }
+
+}
